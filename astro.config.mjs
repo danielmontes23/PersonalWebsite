a7780309d4d6d9a1a05df4a6baea79ece40e19c6
Daniel Montes <129787437+danielmontes23@users.noreply.github.com>
2025-04-13 10:08:47 +0000

    NA

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['/styles/global.css'], // Mark the CSS file as external
+    },
+  },
+});
